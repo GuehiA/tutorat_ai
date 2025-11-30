ba22a66aeab02f8794db76829250f75dc238c48c
ambroiseguehi <ambroiseguehi@gmail.com>
2025-11-30 02:06:25 -0500

    ADD: wkhtmltopdf installation script

---
+++ install_wkhtmltopdf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Installing wkhtmltopdf..."
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+apt-get update
+apt-get install -y ./wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+rm wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+echo "wkhtmltopdf installed successfully"
